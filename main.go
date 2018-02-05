87a48c91dbd1c3741eb1d8cad46e06b07dc5093a
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-02-05 09:37:03 +0200

    Search all repos in SRCPATH

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"regexp"
 	"regexp/syntax"
 	"strings"
@@ -76,26 +77,131 @@ func ripgrep(q query.Q) ([]string, error) {
 	return args, nil
 }
 
-func main() {
-	q, err := query.Parse(strings.Join(os.Args[1:], " "))
-	if err != nil {
-		log.Fatal(err)
-	}
-	q = query.Simplify(q)
-	args, err := ripgrep(q)
-	if err != nil {
-		log.Fatal(err)
+func hasRepoQuery(q query.Q) bool {
+	hasRepo := false
+	query.VisitAtoms(q, func(q query.Q) {
+		if _, ok := q.(*query.Repo); ok {
+			hasRepo = true
+		}
+	})
+	return hasRepo
+}
+
+func runrg(dir string, args []string) int {
+	if dir != "" {
+		args = append(args, "--", dir)
 	}
 	cmd := exec.Command("rg", args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		if e, ok := err.(*exec.ExitError); ok {
 			if ws, ok := e.Sys().(syscall.WaitStatus); ok {
-				os.Exit(ws.ExitStatus())
+				return ws.ExitStatus()
 			}
 		}
 		log.Fatal(err)
+	}
+	return 0
+}
+
+func main() {
+	var args []string
+	if len(args) > 1 {
+		args = os.Args[1:]
+	}
+	if len(args) == 0 || (len(args) == 1 && (args[0] == "--help" || args[1] == "-h")) {
+		code := runrg("", args)
+		fmt.Println()
+		fmt.Printf("USAGE: %s [ripgrep flags...] PATTERN\n", os.Args[0])
+		os.Exit(code)
+	}
+
+	q, err := query.Parse(args[len(args)-1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	q = query.Simplify(q)
+	passthrough := args[:len(args)-1]
+
+	// if we don't have a repo query, root the search from cwd
+	if !hasRepoQuery(q) {
+		args, err := ripgrep(q)
+		if err != nil {
+			log.Fatal(err)
+		}
+		code := runrg("", append(passthrough, args...))
+		os.Exit(code)
+	}
+
+	srcpaths := filepath.SplitList(os.Getenv("SRCPATH"))
+	if len(srcpaths) == 0 {
+		cwd, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+		srcpaths = []string{cwd}
+	}
+
+	hasMatch := false
+	for _, srcpath := range srcpaths {
+		err := filepath.Walk(srcpath, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return nil
+			}
+
+			if !info.IsDir() {
+				return nil
+			}
+
+			if base := filepath.Base(path); len(base) > 0 && base[0] == '.' {
+				return filepath.SkipDir
+			}
+
+			if _, err := os.Stat(filepath.Join(path, ".git")); os.IsNotExist(err) {
+				return nil
+			}
+
+			repo, err := filepath.Rel(srcpath, path)
+			if err != nil {
+				return err
+			}
+
+			// Check if query matches repo
+			q2 := query.Simplify(query.Map(q, func(q query.Q) query.Q {
+				if r, ok := q.(*query.Repo); ok {
+					return &query.Const{Value: strings.Contains(repo, r.Pattern)}
+				}
+				return q
+			}))
+			if c, ok := q2.(*query.Const); ok {
+				if c.Value {
+					hasMatch = true
+					fmt.Println(path)
+				}
+				return filepath.SkipDir
+			}
+
+			args, err := ripgrep(q2)
+			if err != nil {
+				return err
+			}
+			code := runrg(path, append(passthrough, args...))
+			if code == 0 {
+				hasMatch = true
+			} else if code != 1 {
+				os.Exit(code)
+			}
+
+			return filepath.SkipDir
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if !hasMatch {
+		os.Exit(1)
 	}
 }
